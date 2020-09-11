c133e8f504b5de8e39403f7274b31a4ed06d20a0
Lauri Kemppainen <laukempp@gmail.com>
2020-09-11 19:26:12 +0300

    Searchbar modifications

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,13 +1,24 @@
 import React from "react";
 
 class Searchbar extends React.Component {
+  state = {
+    searchTerm: "",
+  };
+
+  onInputChange(e) {
+    this.setState({ searchTerm: e.target.value });
+  }
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
             <label>Image search</label>
-            <input type="text"></input>
+            <input
+              type="text"
+              value={this.state.searchTerm}
+              onChange={this.onInputChange}
+            ></input>
           </div>
         </form>
       </div>
